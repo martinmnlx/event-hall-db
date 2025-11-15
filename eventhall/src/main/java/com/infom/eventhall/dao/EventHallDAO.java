68d6a55bea9e2068541d7782aeb4bb22691bbf34
Sam-dot2 <samuel_bongco@dlsu.edu.ph>
2025-11-16 01:17:38 +0800

    Add files via upload

---
+++ eventhall/src/main/java/com/infom/eventhall/dao/EventHallDAO.java
@@ -0,0 +1,111 @@
+package com.infom.eventhall.dao;
+
+import com.infom.eventhall.model.EventHall;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventHallDAO {
+
+    private final Connection connection;
+
+    public EventHallDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean createEventHall(EventHall eventHall) {
+        String sql = "INSERT INTO Event_Halls (hall_name, capacity, location, status) VALUES (?, ?, ?, ?)";
+
+        // sets all the ? parameters
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setString(1, eventHall.getHallName());
+            stmt.setInt(2, eventHall.getCapacity());
+            stmt.setString(3, eventHall.getLocation());
+            stmt.setString(4, eventHall.getStatus().name());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public EventHall getHallById(int hallId) {
+        String sql = "SELECT * FROM Event_Halls WHERE hall_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, hallId);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                if (rs.next()) {
+                    return mapRowToEventHall(rs);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<EventHall> getAllEventHalls() {
+        List<EventHall> hallList = new ArrayList<>();
+        String sql = "SELECT * FROM Event_Halls";
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()){
+
+            while(rs.next()){
+                hallList.add(mapRowToEventHall(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return hallList;
+    }
+
+    public boolean updateEventHall(EventHall eventHall) {
+        String sql = "UPDATE Event_Halls SET hall_name = ?, capacity = ?, location = ?, status = ? WHERE hall_id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+
+            stmt.setString(1, eventHall.getHallName());
+            stmt.setInt(2, eventHall.getCapacity());
+            stmt.setString(3, eventHall.getLocation());
+            stmt.setString(4, eventHall.getStatus().name());
+            stmt.setInt(5, eventHall.getHallId());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteEventHall(int hallId) {
+        String sql = "DELETE FROM Event_Halls WHERE hall_id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1, hallId);
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private EventHall mapRowToEventHall(ResultSet rs) throws SQLException {
+        EventHall eventHall = new EventHall();
+        eventHall.setHallId(rs.getInt("hall_id"));
+        eventHall.setHallName(rs.getString("hall_name"));
+        eventHall.setCapacity(rs.getInt("capacity"));
+        eventHall.setLocation(rs.getString("location"));
+        eventHall.setStatus(EventHall.HallStatus.valueOf(rs.getString("status")));
+        return eventHall;
+    }
+}
---
+++ eventhall/src/main/java/com/infom/eventhall/dao/ReservationDAO.java
@@ -0,0 +1,204 @@
+package com.infom.eventhall.dao;
+
+import com.infom.eventhall.model.Reservation;
+import com.infom.eventhall.model.User;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationDAO {
+
+    private final Connection connection;
+
+    public ReservationDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean createReservation(Reservation reservation) {
+        String sql = "INSERT INTO Reservations (user_id, hall_id, staff_id, created_on, starts_on, ends_on, event_type, guest_count, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        // sets all the ? parameters
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1, reservation.getUserId());
+            stmt.setInt(2, reservation.getHallId());
+            stmt.setInt(3, reservation.getStaffId());
+            stmt.setTimestamp(4, Timestamp.valueOf(reservation.getCreatedOn()));
+            stmt.setTimestamp(5, Timestamp.valueOf(reservation.getStartsOn()));
+            stmt.setTimestamp(6, Timestamp.valueOf(reservation.getEndsOn()));
+            stmt.setString(7, reservation.getEventType());
+            stmt.setInt(8, reservation.getGuessCount());
+            stmt.setString(9, reservation.getStatus().name());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Reservation getReservationById(int reservationId) {
+        String sql = "SELECT * FROM Reservations WHERE reservation_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, reservationId);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                if (rs.next()) {
+                    return mapRowToReservation(rs);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Reservation> getReservationByUserId(int userId) {
+        List<Reservation> reservationsList = new ArrayList<>();
+        String sql = "SELECT * FROM Reservations WHERE user_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                while (rs.next()) {
+                    reservationsList.add(mapRowToReservation(rs));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reservationsList;
+    }
+
+    public List<Reservation> getReservationByHallId(int hallId) {
+        List<Reservation> reservationsList = new ArrayList<>();
+        String sql = "SELECT * FROM Reservations WHERE hall_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, hallId);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                while (rs.next()) {
+                    reservationsList.add(mapRowToReservation(rs));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reservationsList;
+    }
+
+    public List<Reservation> getReservationByStaffId(int staffId) {
+        List<Reservation> reservationsList = new ArrayList<>();
+        String sql = "SELECT * FROM Reservations WHERE staff_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, staffId);
+
+            try (ResultSet rs = stmt.executeQuery()){
+                while (rs.next()) {
+                    reservationsList.add(mapRowToReservation(rs));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reservationsList;
+    }
+
+    public List<Reservation> getAllReservations() {
+        List<Reservation> reservationsList = new ArrayList<>();
+        String sql = "SELECT * FROM Reservations";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()){
+
+            while(rs.next()){
+                reservationsList.add(mapRowToReservation(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reservationsList;
+    }
+
+    public boolean updateReservation(Reservation reservation) {
+        String sql = "UPDATE Reservations SET user_id = ?, hall_id = ?, staff_id = ?, starts_on = ?, ends_on = ?, event_type = ?, guest_count = ?, status = ? WHERE reservation_id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+
+            stmt.setInt(1, reservation.getUserId());
+            stmt.setInt(2, reservation.getHallId());
+            stmt.setInt(3, reservation.getStaffId());
+            stmt.setTimestamp(4, Timestamp.valueOf(reservation.getStartsOn()));
+            stmt.setTimestamp(5, Timestamp.valueOf(reservation.getEndsOn()));
+            stmt.setString(6, reservation.getEventType());
+            stmt.setInt(7, reservation.getGuessCount());
+            stmt.setString(8, reservation.getStatus().name());
+            stmt.setInt(9, reservation.getReservationId());
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteReservation(int reservationId) {
+        String sql = "DELETE FROM Reservations WHERE reservation_id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1, reservationId);
+
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Integer> findConflictingHallIds(LocalDateTime newStartsOn, LocalDateTime newEndsOn) {
+        List<Integer> conflictingHallIds = new ArrayList<>();
+
+        // This query finds any booking that overlaps with the requested time
+        String sql = "SELECT r.hall_id FROM Reservations r " +
+                "WHERE r.status IN ('Confirmed', 'Pending') " +
+                "AND (r.starts_on < ? AND r.ends_on > ?)"; // (ExistingStart < NewEnd) AND (ExistingEnd > NewStart)
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setTimestamp(1, Timestamp.valueOf(newEndsOn));
+            stmt.setTimestamp(2, Timestamp.valueOf(newStartsOn));
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    conflictingHallIds.add(rs.getInt("hall_id"));
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conflictingHallIds;
+    }
+
+    private Reservation mapRowToReservation(ResultSet rs) throws SQLException {
+        Reservation reservation = new Reservation();
+        reservation.setReservationId(rs.getInt("reservation_id"));
+        reservation.setUserId(rs.getInt("user_id"));
+        reservation.setHallId(rs.getInt("hall_id"));
+        reservation.setStaffId(rs.getInt("staff_id"));
+        reservation.setCreatedOn(rs.getTimestamp("created_on").toLocalDateTime());
+        reservation.setStartsOn(rs.getTimestamp("starts_on").toLocalDateTime());
+        reservation.setEndsOn(rs.getTimestamp("ends_on").toLocalDateTime());
+        reservation.setEventType(rs.getString("event_type"));
+        reservation.setGuessCount(rs.getInt("guess_count"));
+        reservation.setStatus(Reservation.ReservationStatus.valueOf(rs.getString("status")));
+        return reservation;
+    }
+}
